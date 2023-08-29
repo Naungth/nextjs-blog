de26111fd3154d9277baf5947913193da890a2c2
Naungth <thethtoonaung7@gmail.com>
2023-08-28 18:52:53 -0700

    first commit

---
+++ pages/index.js
@@ -0,0 +1,47 @@
+import Head from 'next/head';
+import Layout, { siteTitle } from '../components/layout';
+import utilStyles from '../styles/utils.module.css';
+import Link from 'next/Link';
+import { getSortedPostsData } from '../lib/posts';
+import Date from '../components/date';
+
+export default function Home({allPostsData}) {
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>Trying out NextJS during the Summer break! Here's my{' '}
+          <a href= "https://www.deanza.edu/">college</a>
+        </p>
+        <p>You can Click {' '}
+        <Link href ="/posts/first-post">here</Link> to go to my blogs</p>
+      
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+            <Link href={`/posts/${id}`}>{title}</Link>
+            <br />
+            <small className={utilStyles.lightText}>
+              <Date dateString={date} />
+            </small>
+          </li>
+          ))}
+        </ul>
+      </section>
+    </Layout>
+  );
+}
+
+export async function getStaticProps(){
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
